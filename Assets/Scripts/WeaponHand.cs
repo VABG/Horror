241e67cb2d7456740a2a287e0181570b496fc72e
VABG <VABGProductions@hotmail.com>
2021-05-10 14:14:59 +0200

    Removed HandAnimation requirement.

--- Assets/Scripts/WeaponHand.cs
+++ Assets/Scripts/WeaponHand.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(HandAnimation))]
 public class WeaponHand : MonoBehaviour
 {    
     Animator animator;
